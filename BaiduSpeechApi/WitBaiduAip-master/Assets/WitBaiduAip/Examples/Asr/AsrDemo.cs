56b800b9bfe0e5be23c69d66345c2aa10c3a6f97
warlock07 <956701603@qq.com>
2019-04-22 16:15:03 +0800

    BaiduSpeechApi

---
+++ BaiduSpeechApi/WitBaiduAip-master/Assets/WitBaiduAip/Examples/Asr/AsrDemo.cs
@@ -0,0 +1,63 @@
+﻿/*
+ * 作者：关尔
+ * 时间：2017年12月1日
+ * 关注“洪流学堂”公众号，让你快人几步
+ */
+
+using UnityEngine;
+using UnityEngine.UI;
+using Wit.BaiduAip.Speech;
+
+public class AsrDemo : MonoBehaviour
+{
+    public string APIKey = "";
+    public string SecretKey = "";
+    public Button StartButton;
+    public Button StopButton;
+    public Text DescriptionText;
+
+    private AudioClip _clipRecord;
+    private Asr _asr;
+
+    // Microphone is not supported in Webgl
+#if !UNITY_WEBGL
+
+    void Start()
+    {
+        _asr = new Asr(APIKey, SecretKey);
+        StartCoroutine(_asr.GetAccessToken());
+
+        StartButton.gameObject.SetActive(true);
+        StopButton.gameObject.SetActive(false);
+        DescriptionText.text = "";
+
+        StartButton.onClick.AddListener(OnClickStartButton);
+        StopButton.onClick.AddListener(OnClickStopButton);
+    }
+
+    private void OnClickStartButton()
+    {
+        StartButton.gameObject.SetActive(false);
+        StopButton.gameObject.SetActive(true);
+        DescriptionText.text = "Listening...";
+
+        _clipRecord = Microphone.Start(null, false, 30, 16000);
+    }
+
+    private void OnClickStopButton()
+    {
+        StartButton.gameObject.SetActive(false);
+        StopButton.gameObject.SetActive(false);
+        DescriptionText.text = "Recognizing...";
+        Microphone.End(null);
+        Debug.Log("[WitBaiduAip demo]end record");
+        var data = Asr.ConvertAudioClipToPCM16(_clipRecord);
+        StartCoroutine(_asr.Recognize(data, s =>
+        {
+            DescriptionText.text = s.result != null && s.result.Length > 0 ? s.result[0] : "未识别到声音";
+
+            StartButton.gameObject.SetActive(true);
+        }));
+    }
+#endif
+}
---
+++ BaiduSpeechApi/WitBaiduAip-master/README.md
@@ -0,0 +1,22 @@
+WitBaiduAip
+===
+Unity3d中的百度语音识别和语音合成Restful接口的封装，适用于全平台
+
+本工程基于Unity 2017.3.0f3 (Scripting Runtime Version: .Net 3.5)
+
+工程中包含了2个Demo场景
+- 语音识别，见Assets\WitBaiduAip\Examples\Asr.unity
+- 语音合成，见Assets\WitBaiduAip\Examples\Tts.unity
+
+更新日志
+---
+- 2019年1月12日更新支持了WWW类（可以兼容Unity5.x及以前版本）
+- 2018年8月21日 百度API支持了wav格式，移除第三方插件，直接使用原生API
+- 2018年3月29日 加入了UWP平台支持，移除naudio，使用mp3sharp  
+- 2018年3月28日 加入了平台判断，windows平台使用naudio，android和ios可直接使用mp3  
+- 2018年1月14日 加入了token获取状态的判断
+
+欢迎关注“洪流学堂”微信公众号
+---
+洪流学堂，让你快人几步。  
+![](https://raw.githubusercontent.com/zhenghongzhi/WitBaiduAip/master/%E5%85%B3%E6%B3%A8%E2%80%9C%E6%B4%AA%E6%B5%81%E5%AD%A6%E5%A0%82%E2%80%9D%E5%85%AC%E4%BC%97%E5%8F%B7%EF%BC%8C%E8%AE%A9%E4%BD%A0%E5%BF%AB%E4%BA%BA%E5%87%A0%E6%AD%A5.jpg)  
